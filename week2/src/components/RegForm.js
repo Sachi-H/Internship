db87b1621a918661a646b5a4fd44c4d8af7eb694
HelaenaCambel <frances.helaena@gmail.com>
2024-07-19 13:59:39 +0800

    .

--- week2/src/components/RegForm.js
+++ week2/src/components/RegForm.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 const RegForm = () => {
   return (
-    <div>RegForm</div>
+    <div>
+        <div className='offset-lg-3 col-lg-6'>
+            <form className='container'>
+                <div className='card'>
+                    <div className='card-header'>
+
+                    </div>
+                    <div className='card-body'>
+                        
+                    </div>
+                    <div className='card-footer'>
+                        <button type='submit' className='btn btn-primary'> Register </button>
+                    </div>
+                </div>
+            </form>    
+        </div>
+    </div>
   )
 }
 
