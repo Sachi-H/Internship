21acc953b7d1e8c7f578e4367f11b25e8780dd22
HelaenaCambel <frances.helaena@gmail.com>
2024-07-19 14:54:16 +0800

    .

--- week2/src/App.js
+++ week2/src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import Home from './components/Home';
-import Login from './components/Login';
 import RegForm from './components/RegForm';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />}> </Route>
-          <Route path='/login' element={<Login />}> </Route>
-          <Route path='/registration' element={<RegForm />}> </Route>
-        </Routes>
-      </BrowserRouter>
+      <RegForm />
     </div>
   );
 }
