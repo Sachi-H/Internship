b836ed86c93863a46da28c94f4dbc56806491f67
HelaenaCambel <frances.helaena@gmail.com>
2024-07-19 15:30:36 +0800

    .

--- 1st-task/src/App.js
+++ 1st-task/src/App.js
@@ -1,9 +1,10 @@
+import RegForm from './components/RegForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <RegForm />
     </div>
   );
 }
--- 1st-task/src/components/RegForm.js
+++ 1st-task/src/components/RegForm.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const RegForm = () => {
   return (
-    <div>RegForm</div>
+    <div>
+        <div className='offset-lg-3 col-lg-6'>
+            <form className='container'>
+                <div className='card'>
+                    <div className='card-header'>
+                        <nav style={{ display: 'flex', justifyContent: 'center' }}>
+                            <h1> User Registration </h1>
+                        </nav>
+                    </div>
+                    <div className='card-body'>
+                        
+                    </div>
+                    <div className='card-footer'>
+                        <button type='submit' className='btn btn-primary'> Register </button>
+                    </div>
+                </div>
+            </form>    
+        </div>
+    </div>
   )
 }
 
