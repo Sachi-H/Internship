09d7d87698d26e5f470b8334cdd71210410e6b54
HelaenaCambel <frances.helaena@gmail.com>
2024-07-19 15:54:06 +0800

    .

--- 1st-task/src/components/RegForm.js
+++ 1st-task/src/components/RegForm.js
@@ -4,25 +4,19 @@ const RegForm = () => {
   return (
     <div>
         <div className='offset-lg-3 col-lg-6'>
+            <nav>
+
+            </nav>
             <form className='container'>
-                <div className='card'>
-                    <div className='card-header'>
-                        <nav style={{ display: 'flex', justifyContent: 'center' }}>
-                            <h1> User Registration </h1>
-                        </nav>
-                    </div>
-                    <div className='card-body'>
-                        
-                    </div>
-                    <div className='flex justify-center'>
-                        <button type='submit' className='
-                          px-6 py-3 font-medium text-white  
-                          bg-green-700 hover:bg-green-800
-                          rounded-lg text-center'
-                        > Register </button>
-                    </div>
-                </div>
-            </form>    
+              <h1 style={{ display: 'flex', justifyContent: 'center' }}> User Registration </h1>
+            </form> 
+            <div className='flex justify-center'>
+              <button type='submit' className='
+                px-6 py-3 font-medium text-white  
+                bg-green-700 hover:bg-green-800
+                rounded-lg text-center'
+              > Register </button>
+            </div>   
         </div>
     </div>
   )
