4745535246a4caa795dfdb99bf88b0f0affff46d
Sachi-H <sunvickgencianeo052003@gmail.com>
2024-07-23 17:52:23 +0800

    hjkmhjmhjk

--- 1st-task/src/components/Login.js
+++ 1st-task/src/components/Login.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import{Formik, Field, Form} from 'formik';
-
+import { Formik, Field, Form } from 'formik';
+import * as Yup from 'yup';
 
 const Login = () => {
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email('Invalid email address')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 characters')
+      .required('Password is required')
+  });
+
   return (
     <div className="bg-white dark:bg-gray-900">
       <div className="flex justify-center h-screen">
@@ -27,7 +36,8 @@ const Login = () => {
                         Mission
                       </p>
                       <p className="mt-4 text-lg leading-relaxed text-gray-400 text-left">
-                      Hacktiv exists to democratize deep technologies, making them accessible to all organizations. By doing so, we drive transformative impact and enable businesses throughout the Philippines to harness the power of cutting-edge solutions.                      </p>
+                        Hacktiv exists to democratize deep technologies, making them accessible to all organizations. By doing so, we drive transformative impact and enable businesses throughout the Philippines to harness the power of cutting-edge solutions.
+                      </p>
                     </div>
                     <div className="flex-1 text-center max-w-md">
                       <p className="text-gray-300 text-2xl font-semibold leading-relaxed text-left">
@@ -52,48 +62,66 @@ const Login = () => {
             </div>
 
             <div className="mt-8">
-              <form>
-                <div>
-                  <label htmlFor="email" className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
-                    Email Address
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="example@example.com"
-                    className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                  />
-                </div>
+              <Formik
+                initialValues={{ email: '', password: '' }}
+                validationSchema={validationSchema}
+                onSubmit={async (values) => {
+                  await new Promise((r) => setTimeout(r,500));
+                  alert(JSON.stringify(values, null, 2));
 
-                <div className="mt-6">
-                  <div className="flex justify-between mb-2">
-                    <label htmlFor="password" className="text-sm text-gray-600 dark:text-gray-200">
-                      Password
-                    </label>
-                  </div>
+                }}
+              >
+                {({ errors, touched }) => (
+                  <Form>
+                    <div>
+                      <label htmlFor="email" className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                        Email Address
+                      </label>
+                      <Field
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="example@example.com"
+                        className={`block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border ${errors.email && touched.email ? 'border-red-500' : 'border-gray-200'} rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40`}
+                      />
+                      {errors.email && touched.email ? (
+                        <div className="text-red-500 text-sm mt-1">{errors.email}</div>
+                      ) : null}
+                    </div>
 
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="Your Password"
-                    className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                  />
-                </div>
+                    <div className="mt-6">
+                      <div className="flex justify-between mb-2">
+                        <label htmlFor="password" className="text-sm text-gray-600 dark:text-gray-200">
+                          Password
+                        </label>
+                      </div>
+                      <Field
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="Your Password"
+                        className={`block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border ${errors.password && touched.password ? 'border-red-500' : 'border-gray-200'} rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40`}
+                      />
+                      {errors.password && touched.password ? (
+                        <div className="text-red-500 text-sm mt-1">{errors.password}</div>
+                      ) : null}
+                    </div>
 
-                <div className="mt-6 justify-center">
-                  <button
-                    type="submit"
-                    className=" w-full mt-3 px-6 py-3 tracking-wide font-medium 
-                      bg-[#42bb71] hover:bg-white 
-                      hover:shadow-[inset_0_0_0_2px_#42bb71]
-                      text-white hover:text-[#42bb71]
-                      rounded-lg text-center"    
-                  >Sign in
-                  </button>
-                </div>
-              </form>
+                    <div className="mt-6 justify-center">
+                      <button
+                        type="submit"
+                        className="w-full mt-3 px-6 py-3 tracking-wide font-medium 
+                          bg-[#42bb71] hover:bg-white 
+                          hover:shadow-[inset_0_0_0_2px_#42bb71]
+                          text-white hover:text-[#42bb71]
+                          rounded-lg text-center"
+                      >
+                        Sign in
+                      </button>
+                    </div>
+                  </Form>
+                )}
+              </Formik>
 
               <p className="mt-6 text-sm text-center text-gray-400">
                 Don't have an account yet? <Link to="/register" className="text-blue-500 focus:outline-none focus:underline hover:underline">Sign up</Link>.
--- 1st-task/src/store/actions.js
+++
@@ -1,5 +0,0 @@
-export const saveFormData = (formData) => ({
-    type: 'SAVE_FORM_DATA',
-    payload: formData,
-  });
-  
