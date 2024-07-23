b31e6c4ef9066fd895896d7714d33c2c3f4a1a4c
HelaenaCambel <frances.helaena@gmail.com>
2024-07-23 18:15:17 +0800

    .

--- 1st-task/src/index.js
+++ 1st-task/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import App from './App';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Provider } from 'react-redux';
-import store from './store/store'; // Adjust the path as per your project structure 
-import { createRoot } from 'react-dom';
+import App from './App';
 
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
-
-;
