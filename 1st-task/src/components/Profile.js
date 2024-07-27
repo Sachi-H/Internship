19e37ea0d243d9d437f658248f80087ce9eece44
HelaenaCambel <frances.helaena@gmail.com>
2024-07-27 23:56:57 +0800

    .

--- 1st-task/src/components/Profile.js
+++ 1st-task/src/components/Profile.js
@@ -33,14 +33,67 @@ const Profile = () => {
         <div className='bg-[#42bb71] min-h-[calc(100vh-90px)]'>
           <h1 className='flex items-center justify-center text-white font-bold text-3xl p-5'> User Profile </h1>
 
-          <div className="p-5 text-white font-bold">
-            <p>Name: {currentUser.fullname}</p>
-            <p>Email: {currentUser.email}</p>
-            <p>Phone Number: {currentUser.number}</p>
-            <p>Birthday: {currentUser.birthday}</p>
-            <p>Gender: {currentUser.gender}</p>
-            <p>Address: {currentUser.address}</p>
-            <p>Password: {currentUser.password}</p>
+          <div class="bg-white overflow-hidden shadow rounded-lg border">
+            <div className="border-t border-gray-200 px-4 py-5 sm:p-0 text-white font-bold">
+              <dl className='sm:divide-y sm:divide-gray-200'>
+                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt class="text-sm font-medium text-gray-500">
+                    Full name
+                  </dt>
+                  <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    {currentUser.fullname}
+                  </dd>
+                </div>
+                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt class="text-sm font-medium text-gray-500">
+                    Email address
+                  </dt>
+                  <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    {currentUser.email}
+                  </dd>
+                </div>
+                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt class="text-sm font-medium text-gray-500">
+                    Mobile Number
+                  </dt>
+                  <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    {currentUser.number}
+                  </dd>
+                </div>
+                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt class="text-sm font-medium text-gray-500">
+                    Birthday
+                  </dt>
+                  <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    {currentUser.birthday}
+                  </dd>
+                </div>
+                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt class="text-sm font-medium text-gray-500">
+                    Gender
+                  </dt>
+                  <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    {currentUser.gender}
+                  </dd>
+                </div>
+                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt class="text-sm font-medium text-gray-500">
+                    Address
+                  </dt>
+                  <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    {currentUser.address}
+                  </dd>
+                </div>
+                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt class="text-sm font-medium text-gray-500">
+                    Password
+                  </dt>
+                  <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    {currentUser.password}
+                  </dd>
+                </div>
+              </dl>
+            </div>
           </div>
         </div>
       </div>
@@ -49,3 +102,5 @@ const Profile = () => {
 }
 
 export default Profile
+
+          
