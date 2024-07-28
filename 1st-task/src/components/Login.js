cc73de1a84a88e0a2ae4f5b581f6e5fcca075b19
HelaenaCambel <frances.helaena@gmail.com>
2024-07-28 22:19:21 +0800

    .

--- 1st-task/src/components/Login.js
+++ 1st-task/src/components/Login.js
@@ -56,7 +56,7 @@ const Login = () => {
   };
 
   return (
-    <div className="bg-white dark:bg-gray-900">
+    <div className="bg-white">
       <div className="flex justify-center h-screen">
         <div
           className="hidden bg-cover lg:block lg:w-2/3"
@@ -100,7 +100,7 @@ const Login = () => {
           <div className="flex-1">
             <div className="text-center">
               <img src='https://www.hacktiv.io/wp-content/uploads/2023/10/Logo2.png' alt='Brand Logo' className='w-48 h-auto' />
-              <p className="mt-3 text-gray-500 dark:text-gray-300">Sign in to access your account</p>
+              <p className="mt-3 text-gray-500">Sign in to access your account</p>
             </div>
 
             <div className="mt-8">
@@ -112,7 +112,7 @@ const Login = () => {
                 {({ errors, touched, isSubmitting }) => (
                   <Form>
                     <div>
-                      <label htmlFor="loginCredential" className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                      <label htmlFor="loginCredential" className="block mb-2 text-sm text-gray-600">
                         Email Address or Mobile Number
                       </label>
                       <Field
@@ -120,7 +120,7 @@ const Login = () => {
                         name="loginCredential"
                         id="loginCredential"
                         placeholder="example@example.com or 09123456789"
-                        className={`block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border ${errors.loginCredential && touched.loginCredential ? 'border-red-500' : 'border-gray-200'} rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40`}
+                        className={`block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border ${errors.loginCredential && touched.loginCredential ? 'border-red-500' : 'border-gray-200'} rounded-md focus:border-green-400 focus:ring-green-400 focus:outline-none focus:ring focus:ring-opacity-40`}
                       />
                       {errors.loginCredential && touched.loginCredential && (
                         <div className="text-red-500 text-sm mt-1">{errors.loginCredential}</div>
@@ -129,7 +129,7 @@ const Login = () => {
 
                     <div className="mt-6">
                       <div className="flex justify-between mb-2">
-                        <label htmlFor="password" className="text-sm text-gray-600 dark:text-gray-200">
+                        <label htmlFor="password" className="text-sm text-gray-600">
                           Password
                         </label>
                       </div>
@@ -139,12 +139,12 @@ const Login = () => {
                           name="password"
                           id="password"
                           placeholder="Your Password"
-                          className={`block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border ${errors.password && touched.password ? 'border-red-500' : 'border-gray-200'} rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40`}
+                          className={`block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border ${errors.password && touched.password ? 'border-red-500' : 'border-gray-200'} rounded-md focus:border-green-400 focus:ring-green-400 focus:outline-none focus:ring focus:ring-opacity-40`}
                         />
                         <button
                           type="button"
                           onClick={() => setShowPassword(!showPassword)}
-                          className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-600 dark:text-gray-200"
+                          className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-600"
                         >
                           {showPassword ? (
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -180,7 +180,7 @@ const Login = () => {
               </Formik>
 
               <p className="mt-6 text-sm text-center text-gray-400">
-                Don't have an account yet? <Link to="/register" className="text-blue-500 focus:outline-none focus:underline hover:underline">Sign up</Link>.
+                Don't have an account yet? <Link to="/register" className="text-green-500 focus:outline-none focus:underline hover:underline">Sign up</Link>.
               </p>
             </div>
           </div>
